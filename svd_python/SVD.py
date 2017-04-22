1c3c1ffc88ce62a985d4e00a75a751cb8e5b7181
Kamil Zawilski <zawilskikamil@gmail.com>
2017-04-23 00:30:56 +0200

    wstępna ramy

---
+++ svd_python/SVD.py
@@ -0,0 +1,28 @@
+# matrix - macierz w postaci na przykłąd [[1,2,3][1,2,3][1,2,3]]
+def do_stuff(matrix):
+    # jakaś walidacja czy matrix to macież
+    if not validate(matrix):
+        raise Exception
+
+    # 1. transponujemy macierz
+    #transposed = transposed(matrix)
+
+    # 2. liczymy AT (AAT) i TA (ATA)
+    #TA = matrix_multiplication(matrix,transposed)
+    #AT = matrix_multiplication(matrix,transposed)
+
+    # 3. wybieramy macierz
+    #if (rank(TA) > rank(AT)):
+    #    main_matrix = AT
+    #else:
+    #    main_matrix = TA
+
+    # 4.
+    # 5.
+    # 6.
+    # 7.
+    # 8.
+    # 9.
+
+def validate(matrix):
+    return False
