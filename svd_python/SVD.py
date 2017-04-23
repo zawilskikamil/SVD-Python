e139c7553a9bbd8cefea7e6d431032867590511e
Martyna <martyn97@interia.pl>
2017-04-23 14:05:00 +0200

    1,2,3 nadpisane

--- svd_python/SVD.py
+++ svd_python/SVD.py
@@ -1,21 +1,22 @@
-# matrix - macierz w postaci na przykłąd [[1,2,3][1,2,3][1,2,3]]
+import numpy as np
+A = [[1,2,3],[1,2,3],[1,2,3]]
 def do_stuff(matrix):
-    # jakaś walidacja czy matrix to macież
+    # jakaś walidacja czy matrix to macierz
     if not validate(matrix):
         raise Exception
 
     # 1. transponujemy macierz
-    #transposed = transposed(matrix)
+    T = np.transpose(A)
 
     # 2. liczymy AT (AAT) i TA (ATA)
-    #TA = matrix_multiplication(matrix,transposed)
-    #AT = matrix_multiplication(matrix,transposed)
+    AT = np.dot(A,T)
+    TA = np.dot(T,A)
 
     # 3. wybieramy macierz
-    #if (rank(TA) > rank(AT)):
-    #    main_matrix = AT
-    #else:
-    #    main_matrix = TA
+    if (np.linalg.matrix_rank(TA) > np.linalg.matrix_rank(AT)):
+        main_matrix = AT
+    else:
+        main_matrix = TA
 
     # 4.
     # 5.
