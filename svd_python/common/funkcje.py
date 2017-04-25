d937316574da6782ea51235222d7f141f51456be
zawilskikamil <zawilskikamil@gmail.com>
2017-04-25 13:35:02 +0200

    Update funkcje.py
    poprawione mnorzenie

--- svd_python/common/funkcje.py
+++ svd_python/common/funkcje.py
@@ -6,20 +6,27 @@ def transpozycja(macierz):
 
 
 ## działa tylko dla macierzy kwadratowych
-def mnozenie(macierz1, macierz2):
-    wynik = []
-    for i in range(len(macierz1)):
-        wiersz = macierz1[i]
-        nowyWiersz = []
-        for j in range(len(macierz2[0])):
-            y = 0
-            for x in range(len(wiersz)):
-                wierszE = wiersz[x]
-                kolumnaE = macierz2[x][j]
-                y += wierszE + kolumnaE
-            nowyWiersz.append(y)
-        wynik.append(nowyWiersz)
+def mnozenie(macierz_A, macierz_B):
+    wiersz_A = len(macierz_A)
+    columna_A = len(macierz_A[0])
+    wiersz_B = len(macierz_B)
+    columna_B = len(macierz_B[0])
+
+    if columna_A != columna_B:
+      print "Nie można mnożyć macierzy, ZŁE WYMIARY"
+      return
+
+    # Create the result matrix
+    # Dimensions would be rows_A x cols_B
+    wynik = [[0 for row in range(columna_B)] for col in range(wiersz_A)]
+    print wynik
+
+    for i in range(wiersz_A):
+        for j in range(columna_B):
+            for k in range(columna_A):
+                wynik[i][j] += macierz_A[i][k] * macierz_B[k][j]
     return wynik
+    
 
 
 
