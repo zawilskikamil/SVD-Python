01db9490c3d240afd74bc585837813f123ca09da
Kamil Zawilski <zawilskikamil@gmail.com>
2017-04-27 22:00:03 +0200

    piaskownica - rozwiązywanie wielomianów (z liczbami zespolonymi)

--- svd_python/sandbox/piaskownica.py
+++ svd_python/sandbox/piaskownica.py
@@ -1,6 +1,29 @@
-s = [0] * 7
-print(s)
-s[7] = 2
+import numpy
+from svd_python.common.wielomiany import *
 
 
-print(s)
+
+
+w1 = wielomian()
+w1 = w1 + niewiadoma(1,1)
+w1 = w1 + niewiadoma(-3,0)
+
+w2 = wielomian()
+w2 = w2 + niewiadoma(1,1)
+w2 = w2 + niewiadoma(-1,0)
+
+w3 = wielomian()
+w3 = w3 + niewiadoma(1,1)
+w3 = w3 + niewiadoma(3,0)
+
+w4 = w1 * w2 * w3
+print(w4)
+print(w4.daj_wspolczynniki())
+print(list(reversed(w4.daj_wspolczynniki())))
+coeff = list(reversed(w4.daj_wspolczynniki()))
+d = numpy.roots(coeff)
+print(d)
+
+coeff = [1,2,2]
+d = numpy.roots(coeff)
+print(d)
