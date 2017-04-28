681c469142042bcfe88718d7e21c74ae469575a5
lepamateusz <lepa.mateusz@gmail.com>
2017-04-28 21:07:31 +0200

    Testy do "funkcje.py" c.d.

--- tests/tests_funkcje.py
+++ tests/tests_funkcje.py
@@ -12,6 +12,7 @@ class TestTranspozycja(unittest.TestCase):
         A1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         A2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         wynik = [[30, 36, 42], [66, 81, 96], [102, 126, 150]]
+        self.assertEqual(mnozenie(A1,A2), wynik, "Zły wyznacznik macierzy")
 
     def test_wyznacznik(self):
         A = [[5, 1, 3, 3], [32, 3, 9, 9], [6, 6, 7, 8], [4, -1, -2, -9]]
