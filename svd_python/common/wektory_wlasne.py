7877a56cccb65807312aacf30bcd35d0232f9450
Kamil Zawilski <zawilskikamil@gmail.com>
2017-05-01 01:34:39 +0200

    otrzymujemy macierz V spełniającą warunek V*Vt = I

--- svd_python/common/wektory_wlasne.py
+++ svd_python/common/wektory_wlasne.py
@@ -15,8 +15,12 @@ def daj(macierz):
     wartosci_wlasne = numpy.roots(coeff)
     wartosci_wlasne.sort()
     macierz_wartosci_wlasnych = []
-    for w in wartosci_wlasne:
-        macierz_wartosci_wlasnych.append(daj_wektor_wlasny(w, macierz))
+
+    macierz_wartosci_wlasnych = daj_wektor_wlasny(wartosci_wlasne, macierz)
+    # unormowana = numpy.linalg.norm(macierz_wartosci_wlasnych)
+    # for i in range(len(macierz_wartosci_wlasnych)):
+    #     for j in range(len(macierz_wartosci_wlasnych[i])):
+    #         macierz_wartosci_wlasnych[i][j] = macierz_wartosci_wlasnych[i][j]/unormowana
     return wartosci_wlasne, macierz_wartosci_wlasnych
 
 
@@ -38,25 +42,41 @@ def odejmij_i_lambda(macierz):
     return macierz
 
 
-def daj_wektor_wlasny(lamba, macierz):
+def daj_wektor_wlasny(lamby, macierz):
     kopia_macierzy = copy.deepcopy(macierz)
-    for i in range(len(macierz)):
-        kopia_macierzy[i][i] = kopia_macierzy[i][i] - lamba
-    x0 = [0]*len(kopia_macierzy[0])
-    w = scipy.optimize.root(f, x0=x0, args=kopia_macierzy, method='lm')
-    return w.x
+    args = []
+    for l in lamby:
+        k = copy.deepcopy(kopia_macierzy)
+        for i in range(len(macierz)):
+            k[i][i] = k[i][i] - l
+        args.append(k)
+    x0 = [0]*(len(kopia_macierzy[0])*len(lamby))
+    w = scipy.optimize.root(f, x0=x0, args=args, method='lm')
+    return numpy.array_split(w.x, len(lamby))
 
 
 def f(x,args):
     out = []
-    for j in range(len(args)):
-        o = 0
-        for i in range(len(args[j])):
-            o += (x[i] * args[j][i])
+    for alen in range(len(args)):
+        a = args[alen]
+        for j in range(len(a)):
+            o = 0
+            for i in range(len(a[j])):
+                o += (x[i + alen * len(a[0])] * a[j][i])
+            out.append(o)
+        o = -1
+        for i in range(alen * (len(a[0])), alen * len(a[0]) + len(a[0])):
+            o += (x[i] ** 2)
         out.append(o)
-    o = -1
-    for i in range(len(x)):
-        o += (x[i] ** 2)
-    out.append(o)
+    for k in range(len(args)):
+        for i in range(k+1,len(args)):
+            o = 0
+            for j in range(len(args[0])):
+                x1 = j + k * len(args[0])
+                x2 = j + i*len(args[0])
+                o += x[x1]*x[x2]
+            out.append(o)
     return out
 
+a = daj([[1,2,3],[3,2,3],[1,2,31]])
+print(a)
