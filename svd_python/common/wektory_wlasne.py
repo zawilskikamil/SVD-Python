fd6191abe75327ca1c9bda33a53a7cca4e8eb1ea
Kamil Zawilski <zawilskikamil@gmail.com>
2017-04-30 11:35:11 +0200

    działa dla macierzy 2x2

--- svd_python/common/wektory_wlasne.py
+++ svd_python/common/wektory_wlasne.py
@@ -42,55 +42,21 @@ def daj_wektor_wlasny(lamba, macierz):
     kopia_macierzy = copy.deepcopy(macierz)
     for i in range(len(macierz)):
         kopia_macierzy[i][i] = kopia_macierzy[i][i] - lamba
-    a = numpy.array(kopia_macierzy)
-    b = numpy.array([0] * len(kopia_macierzy))
-    # TODO treba dodać warunek x^2 + y^2 = 1
-    # http://stackoverflow.com/questions/8739227/how-to-solve-a-pair-of-nonlinear-equations-using-python
-    # from scipy.optimize import fsolve
-    # import math
-    #
-    # def equations(p):
-    #     x, y = p
-    #     return (x + y ** 2 - 4, math.exp(x) + x * y - 3)
-    #
-    # x, y = fsolve(equations, (1, 1))
-    #
-    # print equations((x, y))
-    if funkcje.oblicz_wyznacznik(kopia_macierzy) > 0:
-        return numpy.linalg.solve(a, b)
-    else:
-        b = (funkcje.transpozycja(a)[0])
-        for i in range(len(b)):
-            b[i] = - b[i]
-        a = (funkcje.transpozycja(funkcje.transpozycja(a)[1:]))
-        narray = (numpy.linalg.lstsq(a, b, 0)[0])
-        wektor = narray.tolist()
-        # for i in range(len(wektor)):
-        #     if isinstance(wektor[i],numpy.complex):
-        #         pass
-        #     else:
-        #         wektor[i] = float(wektor[i])
-        wektor.insert(0, 1)
-        return wektor
+    x0 = [0]*len(kopia_macierzy[0])
+    w = scipy.optimize.root(f, x0=x0, args=kopia_macierzy, method='lm')
+    return w.x
 
 
-# print(daj([[2,1],[1,2]]))
-
-
-def f(x, args):
+def f(x,args):
     out = []
     for j in range(len(args)):
         o = 0
         for i in range(len(args[j])):
             o += (x[i] * args[j][i])
         out.append(o)
-    o = 0
+    o = -1
     for i in range(len(x)):
         o += (x[i] ** 2)
     out.append(o)
     return out
 
-w = scipy.optimize.root(fun=f, x0=[1, 1], args=[[1, 2],[2,1]], method='excitingmixing', jac=False)
-print(w)
-# w = scipy.optimize.root(f, [0, 0], [[1, 2]])
-# print(w)
--- svd_python/sandbox/wielki_test.py
+++ svd_python/sandbox/wielki_test.py
@@ -1,16 +1,21 @@
 import numpy
 
 from svd_python import SVD
+from svd_python.common import funkcje
 
-A = [[1,1], [0,1], [1,0]]
+A = [[1,2,1], [2,1,1],[1,2,3]]
 q = numpy.linalg.svd(A)
 q2 = SVD.daj_nam_wynik(A)
 print("================")
 print(q[0])
 print(q[1])
 print(q[2])
-
+print(q[2]*q[2])
 print("================")
 print(q2[0])
 print(q2[1])
 print(q2[2])
+print(funkcje.mnozenie(q2[2],funkcje.transpozycja(q2[2])))
+
+asd = [[1,-2],[1,1]]
+print(funkcje.mnozenie(asd,funkcje.transpozycja(asd)))
