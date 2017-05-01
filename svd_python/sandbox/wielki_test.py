b93b9352e311dd0350f3b7977c42c6e803c34c93
Kamil Zawilski <zawilskikamil@gmail.com>
2017-05-01 13:57:49 +0200

    Merge branch 'master' of /home/kamil/ptojekty-git/SVD-Python with conflicts.

--- svd_python/sandbox/wielki_test.py
+++ svd_python/sandbox/wielki_test.py
@@ -1,7 +1,19 @@
-from svd_python.common import wektory_wlasne
+import numpy
 
+from svd_python import SVD
+from svd_python.common import funkcje
 
-
-A = [[3,2],[4,1]]
-
-print(wektory_wlasne.daj(A))
+A = [[1,2,3], [7,3,3],[4,5,6]]
+q = numpy.linalg.svd(A)
+q2 = SVD.daj_nam_wynik(A)
+print("================")
+print(q[0])
+print(q[1])
+print(q[2])
+# print(numpy.dot(numpy.transpose(q[2]),q[2]))
+print("================")
+print(q2[0])
+print(q2[1])
+print(q2[2])
+print("================")
+print(funkcje.mnozenie(q2[2],funkcje.transpozycja(q2[2])))
