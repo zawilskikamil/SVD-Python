4aab845e084f06c57174d0b258f4d4941bb50d82
Kamil Zawilski <zawilskikamil@gmail.com>
2017-05-04 20:13:58 +0200

    dodane komentarze

--- svd_python/SVD.py
+++ svd_python/SVD.py
@@ -14,12 +14,16 @@ def daj_nam_wynik(matrix):
         ([[float]],[[float]],[[float]]): ortonormalna macierz U, macierz diagonalna, macierz Vt - transponowana ortonormalna macierz V
     -------
     """
-
+    #1. Dokonana zostaje transpozycja macierzy wejściowej.
     T = funkcje.transpozycja(matrix)
 
+    #2. Następnie wyliczone zostają macierze ATA i AAT.
     AT = funkcje.mnozenie(matrix, T)
     TA = funkcje.mnozenie(T, matrix)
 
+    #3. W kolejnym etapie zostaje wyliczony rząd dla każdej z utworzonych w poprzednim etapie macierzy.
+    #   Następnie zostają one porównane w celu wybrania macierzy z mniejszym rzędem w celu wybrania właściwej macierzy.
+    #   Zostają również obliczone jej wartości własne i utworzona zostaje macierz V.
     if (numpy.linalg.matrix_rank(TA) > numpy.linalg.matrix_rank(AT)):
        main_matrix = AT
     else:
@@ -27,12 +31,16 @@ def daj_nam_wynik(matrix):
 
     wartosci_wlasne, V = macierz_Vt.stworz_macierz_Vt(main_matrix)
 
+    #4. Następnym krokiem jest stworzenie macierzy ∑ i obliczenie pierwiastków kwadratowych
+    #   z wartości własnych macierzy Aw (czyli macierzy właściwej (ATA lub AAT).
     numrows = len(main_matrix)
     numcols = len(main_matrix[0])
 
     pierwiastki = macierz_E.pierwiastki(wartosci_wlasne)
     E = macierz_E.stworz_macierz_E(pierwiastki, numrows, numcols)
 
+    #5. Ostatnia funkcja składa się z 2 połączonych ze sobą etapów i jest odpowiedzialna
+    #   za stworzenie macierzy U i uzupełnienie jej wartości w zależności od rozmiarów macierzy A.
     r = funkcje.licz_R(wartosci_wlasne)
 
     U = macierz_U.stworz_macierz_U(pierwiastki, matrix, V, r)
--- svd_python/sandbox/wielki_test.py
+++ svd_python/sandbox/wielki_test.py
@@ -3,14 +3,13 @@ import numpy
 from svd_python import SVD
 from svd_python.common import funkcje
 
-A = [[4,12,3],[11,3,32],[-3,-33,2]]
-q = numpy.linalg.svd(A)
+A = [[4,12,3],[-11,3,32],[-3,-33,2]]
+nU, nE, nVt = numpy.linalg.svd(A)
 U, E, Vt = SVD.daj_nam_wynik(A)
 print("================")
-print(q[0])
-print(q[1])
-print(q[2])
-# print(numpy.dot(numpy.transpose(q[2]),q[2]))
+print(nU)
+print(nE)
+print(nVt)
 print("================")
 print(U)
 print(E)
